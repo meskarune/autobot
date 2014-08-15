d846a2cf3dca12c343796f2c68abd7f7f6542f9b
Zack Buhman <zack@buhman.org>
2014-08-14 21:27:07 -0400

    add epoll'ed socket handling

--- autobot.py
+++ autobot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import configparser, socket, ssl, time
+import select
 import irc.bot
 from threading import Thread
 
@@ -84,20 +85,35 @@ class TCPinput (Thread):
         self.listenport = listenport
         self.connection = connection
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((listenhost, listenport))
-        self.socket.listen(10)
+        self.accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.accept_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.accept_socket.bind((listenhost, listenport))
+        self.accept_socket.listen(10)
+
+        self.accept_socket.setblocking(False)
+
+        self.epoll = select.epoll()
+        self.epoll.register(self.accept_socket.fileno(), select.EPOLLIN)
+
+        self.stuff = {}
 
     def run(self):
-        while 1:
-            conn, addr = self.socket.accept()
-            data, self.listenport = conn.recvfrom(1024)
-            print(data)
-            if data:
-                self.AutoBot.announce(self.connection, data)
-            else:
-                self.socket.close()
+        while True:
+            for sfd, ev in self.epoll.poll():
+                if sfd == self.accept_socket.fileno():
+                    conn, addr = self.accept_socket.accept()
+                    self.epoll.register(conn.fileno(), select.EPOLLIN)
+                    self.stuff[conn.fileno()] = conn
+
+                else:
+                    conn = self.stuff[sfd]
+
+                    buf = conn.recv(1024)                
+                    if not buf:
+                        conn.close()
+                        continue
+
+                    self.AutoBot.announce(self.connection, buf)
 
 def main():
     config = configparser.ConfigParser()
