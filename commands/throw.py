270707e0ed738912643abe766092d7ffb61ee43a
Sebastian Reuße <github.apriori@spamgourmet.com>
2015-05-17 20:37:51 +0200

    Add command to throw stuff at folks.
    Command syntax is !throw <class> <person>, where <class> is a
    category of things. E. g., «cat», randomly yields things like
    «a sabertooth tiger» or «a snow leopard».
    
    Upon first import, this will install the WordNet database into
    the users home folder.

---
+++ commands/throw.py
@@ -0,0 +1,41 @@
+import random
+
+import nltk
+from nltk.corpus import wordnet
+nltk.download("wordnet")
+
+
+def get_hyponym(query):
+    """ Return a random hyponym for query.
+    """
+
+    thing_lemmas = wordnet.lemmas(query, pos=wordnet.NOUN)
+    thing_senses = [
+        lemma.synset()
+        for lemma in thing_lemmas
+    ]
+
+    if not thing_senses:
+        raise ValueError("No results for query {}".format(query))
+
+    def get_hyponym(lemma):
+        return lemma.hyponyms()
+
+    hyponyms = [
+        hyponym_lemma.name()
+        for thing_synset in thing_senses
+        for hyponym_synset in thing_synset.closure(get_hyponym)
+        for hyponym_lemma in hyponym_synset.lemmas()
+    ]
+
+    if not hyponyms:
+        raise ValueError("No hyponyms for query {}".format(query))
+
+    # Fix up whitespace.
+    hyponyms = list(map(lambda lexeme: lexeme.replace("_", " "), hyponyms))
+    choice = random.choice(hyponyms)
+
+    # Add determiner.
+    det = "an" if choice[0] in "aeiou" else "a"
+
+    return "{det} {choice}".format(**locals())
