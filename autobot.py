5caa72e4582da92563eee696cca74c8bf2827663
meskarune <admin@doloresportalatin.info>
2015-10-03 22:24:45 -0400

    cleaned up code to match pep8, added on_mode

--- autobot.py
+++ autobot.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A full featured python IRC bot"""
 
 import configparser, socket, ssl, time, datetime
 import select
-import re
 import irc.bot
 from threading import Thread
 
 # Create our bot class
-class AutoBot ( irc.bot.SingleServerIRCBot ):
+class AutoBot(irc.bot.SingleServerIRCBot):
+    """Create the single server irc bot"""
     def __init__(self, nick, name, nickpass, channels, network, listenhost, listenport, port=6667, usessl=False):
+        """Connect to the IRC server"""
         if usessl:
             factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
         else:
@@ -23,10 +26,12 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
         self.inputthread = TCPinput(self.connection, self, listenhost, listenport)
         self.inputthread.start()
 
-    def on_nicknameinuse(self, connection, event):
+    def on_nicknameinuse(self, connection):
+        """If the nick is in use, get nick_"""
         connection.nick(connection.get_nickname() + "_")
 
-    def on_welcome ( self, connection, event ):
+    def on_welcome(self, connection):
+        """Join channels and regain nick"""
         for channel in self.channel_list:
             connection.join(channel)
             self.logmessage("autobot", "info", "Joined channel %s" % (channel))
@@ -35,10 +40,11 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
             self.logmessage("autobot", "nickserv", "Recovered nick")
 
     def on_privnotice(self, connection, event):
+        """Identify to nickserv"""
         if not event.source:
             return
         source = event.source.nick
-        if source and source.lower()  == "nickserv":
+        if source and source.lower() == "nickserv":
             if event.arguments[0].lower().find("identify") >= 0:
                 if self.nickpass and self.nick == connection.get_nickname():
                     connection.privmsg("nickserv", "identify %s %s" % (self.nick, self.nickpass))
@@ -48,6 +54,7 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
         #wait 60 seconds and try to reconnect? or exit?
 
     def on_kick(self, connection, event):
+        """Log kicked nicks and rejoin channels if bot is kicked"""
         channel = event.target
         kickedNick = event.arguments[0]
         kicker = event.source.nick
@@ -57,23 +64,40 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
             for channel in self.channel_list:
                 connection.join(channel)
 
-    def on_quit(self, connection, event):
-        channel = event.target
+    def on_quit(self, event):
+        """Log when users quit"""
         nick = event.source.nick
-        self.logmessage(channel, "info", "%s has quit" % (nick))
+        host = event.source.host
+        for channel in self.channel_list:
+            if self.channels[channel].has_user(nick):
+                self.logmessage(channel, "info", "%s@%s has quit" % (nick, host))
 
-    def on_join(self, connection, event):
+    def on_join(self, event):
+        """Log channel joins"""
         channel = event.target
         nick = event.source.nick
         host = event.source.host
-        self.logmessage(channel, "info", "%s@%s joined the channel" % (nick,host))
+        self.logmessage(channel, "info", "%s@%s joined the channel" % (nick, host))
 
-    def on_part(self, connection, event):
+    def on_part(self, event):
+        """Log channel parts"""
         channel = event.target
         nick = event.source.nick
-        self.logmessage(channel, "info", "%s left the channel" % (nick))
+        host = event.source.host
+        self.logmessage(channel, "info", "%s@%s left the channel" % (nick, host))
 
-    def on_pubmsg (self, connection, event):
+    def on_nick(self, event):
+        """Log nick changes"""
+        nick = event.source.nick
+        newnick = event.target
+        host = event.source.host
+        self.logmessage("autobot", "info", "%s@%s changed their nick to %s" %(nick, host, newnick))
+
+    def on_mode(self, event):
+        """Log mode changes"""
+
+    def on_pubmsg(self, connection, event):
+        """Log public messages and respond to command requests"""
         channel = event.target
         nick = event.source.nick
         message = event.arguments[0]
@@ -83,12 +107,20 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
                 self.do_command(event, True, channel, message.lstrip("!").lower())
             else:
                 self.do_command(event, False, channel, message.lstrip("!").lower())
+        if message.startswith(self.nick):
+            connection.privmsg(channel, "hello " + nick + ", I am a bot.")
 
-    def on_privmsg(self, connection, event):
+    def on_privmsg(self, event):
+        """Log private messages and respond to command requests"""
+        channel = event.target
+        nick = event.source.nick
+        message = event.arguments[0]
+        self.logmessage(channel, nick, message)
         if event.arguments[0].startswith("!"):
-            self.do_command(event, False, event.source.nick, event.arguments[0].lstrip("!").lower())
+            self.do_command(event, False, nick, message.lstrip("!").lower())
 
-    def do_command (self, event, isOper, source, command):
+    def do_command(self, event, isOper, source, command):
+        """Commands the bot will respond to"""
         user = event.source.nick
         connection = self.connection
         if command == "hello":
@@ -102,7 +134,8 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
         elif command == "slap":
             connection.action(source, "slaps " + user + " around a bit with a large trout")
         elif command == "help":
-            connection.privmsg(source, "Available commands: ![hello, goodbye, ugm, ugn, slap, disconnect, die, help]")
+            connection.privmsg(source, "Available commands: ![hello, goodbye,"
+                                       "ugm, ugn, slap, disconnect, die, help]")
         elif command == "disconnect":
             if isOper:
                 self.disconnect(msg="I'll be back!")
@@ -114,18 +147,21 @@ class AutoBot ( irc.bot.SingleServerIRCBot ):
             else:
                 connection.privmsg(source, "You don't have permission to do that")
         else:
-            connection.notice( user, "I'm sorry, " + user + ". I'm afraid I can't do that")
+            connection.notice(user, "I'm sorry, " + user + ". I'm afraid I can't do that")
 
-    def announce (self, connection, text):
+    def announce(self, connection, text):
+        """Send notice to joined channels"""
         for channel in self.channel_list:
             connection.notice(channel, text)
 
-    def logmessage (self, channel, nick, message):
+    def logmessage(self, channel, nick, message):
+        """Create IRC logs"""
         timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
         with open(channel + '.log', 'a') as log:
             log.write("%s <%s> %s\n" % (timestamp, nick, message))
 
-class TCPinput (Thread):
+class TCPinput(Thread):
+    """Listen for data on a port and send it to Autobot.announce"""
     def __init__(self, connection, AutoBot, listenhost, listenport):
         Thread.__init__(self)
         self.setDaemon(1)
@@ -179,7 +215,7 @@ def main():
     #FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
     #logging.basicConfig(format=FORMAT)
 
-    bot = AutoBot (nick, name, nickpass, channels, network, listenhost, listenport, port, _ssl)
+    bot = AutoBot(nick, name, nickpass, channels, network, listenhost, listenport, port, _ssl)
     bot.start()
 
 if __name__ == "__main__":
