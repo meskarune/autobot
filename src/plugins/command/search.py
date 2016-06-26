a469937ba317f7b22c4e6b2b39188400d6efc613
Dolores Portalatin <meskarune@juno.doloresportalatin.info>
2016-06-26 11:07:44 -0400

    fixed syntax error

--- src/plugins/command/search.py
+++ src/plugins/command/search.py
@@ -22,8 +22,7 @@ def ddg(search):
             return
     else:
         try:
-            site =
-            get("http://duckduckgo.com/lite/?q={0}&kl=us-en".format(search + "%20-site:r.search.yahoo.com")).text
+            site = get("http://duckduckgo.com/lite/?q={0}&kl=us-en".format(search + "%20-site:r.search.yahoo.com")).text
         except:
             return
         try:
