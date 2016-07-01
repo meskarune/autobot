59369da8b07a412fde8b9cead92a44b34cada125
Dolores Portalatin <meskarune@juno.doloresportalatin.info>
2016-07-01 01:47:06 -0400

    tester script for socketserver

---
+++ src/pytcp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import socket
+import threading
+import socketserver
+
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    """ Echo data back in uppercase """
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        if not data:
+            self.request.close()
+        response = bytes("{0}".format(data), 'ascii')
+        self.request.sendall(response.upper())
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    server.serve_forever()
