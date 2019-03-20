4079a5004554e7ca63d77ed762275092cecd3547
Dolores Portalatin <meskarune@gmail.com>
2019-03-20 01:57:00 -0400

    updated the weather plugin so its better

--- src/plugins/command/weather.py
+++ src/plugins/command/weather.py
@@ -8,24 +8,62 @@ from urllib.parse import quote_plus
 from requests import get
 
 
-def getweather(city, location=''):
+def parseweather(url, units):
+    icon_list = {
+        "01": "☀",
+        "02": "🌤",
+        "03": "🌥",
+        "04": "☁",
+        "09": "🌧",
+        "10": "🌦",
+        "11": "🌩",
+        "13": "🌨",
+        "50": "🌫",
+    }
+    if units == "imperial":
+        measure = "°F"
+        speed = "mph"
+    else:
+        measure = "°C"
+        speed = "kph"
+    try:
+        weather_response = json.loads(get(url).text)
+        if weather_response:
+            location = weather_response["name"]
+            conditions = weather_response["weather"][0]["main"]
+            tempurature = float(weather_response['main']['temp'])
+            icon = weather_response["weather"][0]["icon"]
+            humidity = float(weather_response['main']['humidity'])
+            windspeed = weather_response['wind']['speed']
+            if tempurature:
+                return "The current weather for {0} is {1}{2} {3}  {4}, {5}% humidity with {6}{7} winds".format(
+                    location, round(tempurature), measure,
+                    icon_list.get(icon[:2]), conditions, round(humidity),
+                    round(windspeed), speed)
+    except:
+        return "There was an error getting the weather data."
+
+def getweather(location):
     """Get wearther and return the result"""
     config = configparser.ConfigParser()
     config.read("plugins/command/weather.conf")
-    api = config.get("Key", "api_key")
+    geocode_api = config.get("Key", "api_key_geocode")
+    weather_api = config.get("Key", "api_key_weather")
+    geocode_url = "http://open.mapquestapi.com/geocoding/v1/address?key={0}&location={1}".format(
+        geocode_api, quote_plus(location))
     try:
-        query = "http://api.openweathermap.org/data/2.5/weather?q={0},{1}&units=imperial&APPID={2}".format(quote_plus(city), quote_plus(location), api)
-        response = json.loads(get(query).text)
-        if response:
-            location = response['name']
-            conditions = response["weather"][0]["main"]
-            tempurature = float(response['main']['temp'])
-            tempC = (tempurature - 32.0) / (9.0/5.0)
-            humidity = float(response['main']['humidity'])
-            windspeed = response['wind']['speed']
-            if tempurature:
-                return "The current weather for {0} is {1}°F ({2}°C) {3} {4}% humidity with {5}mph winds".format(location, round(tempurature), round(tempC), conditions, round(humidity), round(windspeed))
+        geocode_response = json.loads(get(geocode_url).text)
+        if geocode_response:
+            lat = geocode_response["results"][0]["locations"][0]["latLng"]["lat"]
+            lon = geocode_response["results"][0]["locations"][0]["latLng"]["lng"]
+            country = geocode_response["results"][0]["locations"][0]["adminArea1"]
+            if country == "US":
+                cf = "imperial"
             else:
-                return "Not found, please try again with new parameters"
+                cf = "metric"
     except:
         return
+    weather_url = "https://api.openweathermap.org/data/2.5/weather?appid={0}&units={1}&lat={2}&lon={3}".format(
+        weather_api, cf, lat, lon)
+    reply = parseweather(weather_url, cf)
+    return reply
